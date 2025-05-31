11d1af7f9b15c94c14d27af0a0cac9be9b6426aa
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-05-31 23:59:41 +0200

    feat: add Headline component and update LandingPage layout

--- packages/frontend/src/app/(landing)/page.tsx
+++ packages/frontend/src/app/(landing)/page.tsx
@@ -1,7 +1,12 @@
+import { Headline } from "@/components/headline"
+
 export default function LandingPage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-[#e8f0ff] to-[#bad0ee] px-4 pb-20">
-      <h1 className="text-4xl font-bold">Welcome to the Landing Page</h1>
+      <div className="relative mx-auto mt-10 flex w-full flex-col items-center gap-y-8 lg:mt-16">
+        <Headline />
+        {/* <GetStartedButton /> */}
+      </div>
     </main>
   )
 }
---
+++ packages/frontend/src/components/headline.tsx
@@ -0,0 +1,14 @@
+export const Headline = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-center text-4xl leading-[1.15] font-extrabold tracking-tighter text-pretty lg:text-5xl">
+        Engage your audience like never before
+      </h1>
+
+      <p className="text-muted-foreground lg:mt-4l mt-2 text-center leading-relaxed">
+        Pulse offers a suite of interactive features to keep your audience engaged nad provide you with valuable
+        insights.
+      </p>
+    </div>
+  )
+}
