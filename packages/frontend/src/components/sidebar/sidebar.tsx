80edba248efb04a7f4ac3fce47e7f58eb7940092
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-06-07 19:37:50 +0200

    fix: update sidebar component to hide by default

--- packages/frontend/src/components/sidebar/sidebar.tsx
+++ packages/frontend/src/components/sidebar/sidebar.tsx
@@ -4,7 +4,7 @@ import { Content } from "./content"
 export const Sidebar = () => {
   return (
     <aside
-      className={cn("h-full shrink-0 grow-0 border border-r-0 border-r-blue-500/30 bg-white lg:block lg:basis-[250px]")}
+      className={cn("hidden h-full shrink-0 grow-0 border-r border-r-blue-500/30 bg-white lg:block lg:basis-[250px]")}
     >
       <Content />
     </aside>
