effcef3c16966828d97c8ab25a8de43835d79733
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-06-10 18:26:40 +0200

    feat: add button to create new events in the event list component

--- packages/frontend/src/components/event-list.tsx
+++ packages/frontend/src/components/event-list.tsx
@@ -3,6 +3,7 @@
 import { eventListQueryOptions } from "@/api/event"
 import { useSuspenseQuery } from "@tanstack/react-query"
 import { EventCard } from "@/components/event-card"
+import { PlusIcon } from "lucide-react"
 
 export const EventList = () => {
   const { data } = useSuspenseQuery(eventListQueryOptions())
@@ -10,6 +11,12 @@ export const EventList = () => {
   return (
     <div className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3">
       {/* <div>{JSON.stringify(data)}</div> */}
+      <button className="group grid min-h-24 grow border-spacing-4 place-items-center border-[3px] border-dashed bg-white/50">
+        <span className="inline-flex flex-col items-center gap-x-1 font-medium transition-colors duration-150 group-hover:text-blue-600">
+          <PlusIcon className="size-5" />
+          <span>New Event</span>
+        </span>
+      </button>
 
       {data.eventItems.map((event) => (
         <EventCard key={event.id} event={event} />
