e444aa95dda932eea9cc85e8dac32224fe1821f9
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-06-14 16:19:48 +0200

    feat: add endpoint to retrieve bookmarked event items with pagination

--- packages/backend/src/routes/event.ts
+++ packages/backend/src/routes/event.ts
@@ -3,7 +3,13 @@ import type { Context } from "../utils/context"
 import { signedIn } from "../middleware/signed-in"
 import { zValidator } from "@hono/zod-validator"
 import { paginationSchema } from "database/src/validators/pagination"
-import { getEventItemsCount, getEventItems, getEventItem } from "database/src/queries/event"
+import {
+  getEventItemsCount,
+  getEventItems,
+  getEventItem,
+  getBookmarkedEventItemsCount,
+  getBookmarkedEventItems,
+} from "database/src/queries/event"
 import type { User } from "database/src/drizzle/schema/auth"
 import type { Event } from "database/src/drizzle/schema/event"
 import type { ErrorResponse, PaginatedSuccessResponse, SuccessResponse } from "database/src/types"
@@ -23,6 +29,21 @@ eventRoute
       success: true,
       data: eventItems,
       message: "Event items retrieved",
+      pagination: { page, totalPages: Math.ceil(eventCount / limit), totalItems: eventCount },
+    })
+  })
+  .get("/bookmarked", signedIn, zValidator("query", paginationSchema), async (c) => {
+    const query = c.req.valid("query")
+    const { page, limit } = query
+    const user = c.get("user") as User
+
+    const eventCount = await getBookmarkedEventItemsCount({ userId: user.id })
+    const eventItems = await getBookmarkedEventItems({ userId: user.id, ...query })
+
+    return c.json<PaginatedSuccessResponse<Event[]>>({
+      success: true,
+      data: eventItems,
+      message: "Bookmarked event items retrieved",
       pagination: { page, totalPages: Math.ceil(eventCount / limit), totalItems: eventCount },
     })
   })
