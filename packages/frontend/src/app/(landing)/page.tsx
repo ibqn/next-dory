0c5c5322bc59364861988a044e4b31bb1e282ddd
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-06-15 11:19:01 +0200

    feat: add ThemeToggle component to LandingPage for theme selection

--- packages/frontend/src/app/(landing)/page.tsx
+++ packages/frontend/src/app/(landing)/page.tsx
@@ -2,6 +2,7 @@ import { AuthLoader } from "@/components/auth-loader"
 import { AuthButtons } from "@/components/buttons/auth-buttons"
 import { GetStartedButton } from "@/components/buttons/get-started-button"
 import { Headline } from "@/components/headline"
+import { ThemeToggle } from "@/components/theme-toggle"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import { ValuePropositions } from "@/components/value-propositions"
 import Image from "next/image"
@@ -9,8 +10,9 @@ import { Suspense } from "react"
 
 export default function LandingPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-br from-[#e8f0ff] to-[#bad0ee] px-4 pb-20">
-      <nav className="mt-6 ml-auto">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-br from-[#e8f0ff] to-[#bad0ee] px-4 pb-20 dark:from-[#3B4A66] dark:to-[#283C55]">
+      <nav className="mt-6 ml-auto inline-flex items-center gap-4">
+        <ThemeToggle />
         <Suspense fallback={<AuthLoader />}>
           <AuthButtons />
         </Suspense>
