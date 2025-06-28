2c377a22aa33db27bf871ac24f289b3ced6a0f7e
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-06-28 16:24:20 +0200

    fix: update user query options in GetStartedButton component for improved data handling

--- packages/frontend/src/components/buttons/get-started-button.tsx
+++ packages/frontend/src/components/buttons/get-started-button.tsx
@@ -4,21 +4,21 @@ import Link from "next/link"
 import { buttonVariants } from "@/components/ui/button"
 import { cn } from "@/lib/class-names"
 import { useQuery } from "@tanstack/react-query"
-import { userQueryOptions } from "@/api/auth"
+import { validateQueryOptions } from "@/api/auth"
 import { useEffect, useState } from "react"
 import { Route } from "@/routes"
 
 export const GetStartedButton = () => {
-  const { data: user } = useQuery(userQueryOptions())
+  const { data } = useQuery(validateQueryOptions())
   const [href, setHref] = useState<string>(Route.signIn)
 
   useEffect(() => {
-    if (user) {
+    if (data?.user) {
       setHref(Route.dashboard)
     } else {
       setHref(Route.signIn)
     }
-  }, [user])
+  }, [data])
 
   return (
     <Link href={href} className={cn(buttonVariants(), "rounded-sm p-6 text-sm lg:p-8 lg:text-xl")}>
