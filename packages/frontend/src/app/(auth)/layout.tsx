506fd9a88667cfa362dd7cde868b52865b0c3143
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-10-18 13:44:03 +0200

    refactor: simplify AuthLayout component return statement

--- packages/frontend/src/app/(auth)/layout.tsx
+++ packages/frontend/src/app/(auth)/layout.tsx
@@ -1,13 +1,9 @@
-import { ReactNode } from 'react'
+import type { ReactNode } from "react"
 
 type Props = Readonly<{
   children: ReactNode
 }>
 
 export default function AuthLayout({ children }: Props) {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      {children}
-    </div>
-  )
+  return <div className="flex min-h-screen items-center justify-center">{children}</div>
 }
