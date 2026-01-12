12500e289f2ea40b0799fc4472b3a0eff6305e43
Evgeny Bobkin <evgeny.bobkin@europace.de>
2026-01-12 11:49:58 +0100

    feat: make AuthLayout asynchronous to handle user validation before rendering

--- packages/frontend/src/app/(auth)/layout.tsx
+++ packages/frontend/src/app/(auth)/layout.tsx
@@ -1,9 +1,18 @@
+import { validateRequest } from "@/auth"
+import { Route } from "@/routes"
+import { redirect } from "next/navigation"
 import type { ReactNode } from "react"
 
 type Props = Readonly<{
   children: ReactNode
 }>
 
-export default function AuthLayout({ children }: Props) {
+export default async function AuthLayout({ children }: Props) {
+  const { user } = await validateRequest()
+
+  if (user) {
+    redirect(Route.dashboard)
+  }
+
   return <div className="flex min-h-screen items-center justify-center">{children}</div>
 }
