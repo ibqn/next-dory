247e73d9f68f6decadd0277216d0fe0b22b3a032
Evgeny Bobkin <evgeny.bobkin@europace.de>
2026-01-23 11:01:23 +0100

    feat: enhance EventLayoutServer to prefetch validation query alongside event query

--- packages/frontend/src/app/(app)/events/[eventId]/layout.tsx
+++ packages/frontend/src/app/(app)/events/[eventId]/layout.tsx
@@ -2,6 +2,7 @@ import { Suspense, type PropsWithChildren } from "react"
 import { eventQueryOptions } from "@/api/event"
 import { getQueryClient } from "@/lib/query-client"
 import { EventLayout } from "@/components/layout/event-layout"
+import { validateQueryOptions } from "@/api/auth"
 
 type Props = PropsWithChildren<{
   params: Promise<{
@@ -13,7 +14,10 @@ export default async function EventLayoutServer({ children, params }: Props) {
   const { eventId } = await params
 
   const queryClient = getQueryClient()
-  await queryClient.prefetchQuery(eventQueryOptions({ id: eventId }))
+  await Promise.allSettled([
+    queryClient.prefetchQuery(eventQueryOptions({ id: eventId })),
+    queryClient.prefetchQuery(validateQueryOptions()),
+  ])
 
   return (
     <Suspense>
