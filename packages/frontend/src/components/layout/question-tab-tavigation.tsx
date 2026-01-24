597bc69ab942ae2467e7919415c57242cf41f005
Evgeny Bobkin <evgeny.bobkin@europace.de>
2026-01-24 11:24:50 +0100

    refactor: simplify tab navigation logic and remove unused route functions

--- packages/frontend/src/components/layout/question-tab-tavigation.tsx
+++ packages/frontend/src/components/layout/question-tab-tavigation.tsx
@@ -1,11 +1,9 @@
 "use client"
 
 import { QuestionPageQueryParams } from "@/config/question-page-query-params"
-import { RouteFunctions } from "@/routes"
 import { useParams, usePathname, useRouter, useSearchParams } from "next/navigation"
 import { NavTabButton } from "@/components/buttons/nav-tab-button"
 import { useMemo } from "react"
-import qs from "query-string"
 
 const QuestionTab = {
   open: "open",
@@ -26,32 +24,20 @@ export const QuestionTabNavigation = () => {
 
   const showResolved = searchParams.get(QuestionPageQueryParams.resolved) === "true"
 
-  const eventQuestionRoute = RouteFunctions.event({
-    eventSlug,
-    eventId,
-  })
-
   const activeTab: QuestionTabs = useMemo(
-    () => (pathname === eventQuestionRoute && !showResolved ? QuestionTab.open : QuestionTab.resolved),
-    [pathname, eventQuestionRoute, showResolved]
+    () => (!showResolved ? QuestionTab.open : QuestionTab.resolved),
+    [showResolved]
   )
 
   const handleTabChange = (tab: QuestionTabs) => {
-    const url = qs.stringifyUrl(
-      {
-        url: eventQuestionRoute,
-        query: {
-          [QuestionPageQueryParams.resolved]: tab === QuestionTab.resolved,
-        },
-      },
-      { skipNull: true, skipEmptyString: true }
-    )
+    const newParams = new URLSearchParams(searchParams)
 
-    router.replace(url)
+    newParams.set(QuestionPageQueryParams.resolved, String(tab === QuestionTab.resolved))
+    router.replace(`${pathname}?${newParams.toString()}`)
   }
 
   return (
-    <nav className="inline-flex">
+    <nav className="inline-flex gap-1">
       <NavTabButton isActive={activeTab === QuestionTab.open} onClick={() => handleTabChange(QuestionTab.open)}>
         Open
       </NavTabButton>
